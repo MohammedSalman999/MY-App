8f91ef1b257c266862a446edab4335851f4e915f
MOHAMMED SALMAN <130473682+MohammedSalman999@users.noreply.github.com>
2024-05-24 17:53:15 +0530

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,41 @@
+/*=============== SHOW HIDDEN - PASSWORD ===============*/
+const showHiddenPass = (loginPass, loginEye) => {
+  const input = document.getElementById(loginPass),
+    iconEye = document.getElementById(loginEye);
+
+  iconEye.addEventListener("click", () => {
+    // Change password to text
+    if (input.type === "password") {
+      // Switch to text
+      input.type = "text";
+
+      // Icon change
+      iconEye.classList.add("ri-eye-line");
+      iconEye.classList.remove("ri-eye-off-line");
+    } else {
+      // Change to password
+      input.type = "password";
+
+      // Icon change
+      iconEye.classList.remove("ri-eye-line");
+      iconEye.classList.add("ri-eye-off-line");
+    }
+  });
+};
+
+showHiddenPass("login-pass", "login-eye");
+
+// js for the photo page tmpl
+
+let next = document.querySelector(".next");
+let prev = document.querySelector(".prev");
+
+next.addEventListener("click", function () {
+  let items = document.querySelectorAll(".item");
+  document.querySelector(".slide").appendChild(items[0]);
+});
+
+prev.addEventListener("click", function () {
+  let items = document.querySelectorAll(".item");
+  document.querySelector(".slide").prepend(items[items.length - 1]); // here the length of items = 6
+});
